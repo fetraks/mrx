db10dbc5d307d6904e13ebddfa18794c986f49ce
Fernando Fujie <fernando.fujie@gmail.com>
2013-02-07 19:23:25 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrx::Application.config.secret_token = '8041adfc4b05dc052fcd518a39bd0d199d09dba6b777999324ae3c840b192610655f6f066dc26a524f1f2833e4b52d08606bc558538f756128e926456ec73297'
